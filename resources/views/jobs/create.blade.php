ad732587f831d202246d97dacba5a5b42c4b8aae
Abdekrahman Khakifa <bodakhalifa707@gmail.com>
2025-06-03 16:35:13 +0300

    Partials & include

--- resources/views/jobs/create.blade.php
+++ resources/views/jobs/create.blade.php
@@ -1,19 +1,9 @@
-<!doctype html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Create Job</title>
-</head>
-<body>
-<h1>Create New job</h1>
-<form action="/jobs" method="post">
-    @csrf
-    <input type="text" name="title" placeholder="Job Title">
-    <input type="text" name="description" placeholder="Job Description">
-    <button type="submit">Create Job</button>
-</form>
-</body>
-</html>
+@extends('layout') @section('content')
+    <h1>Create Job</h1>
+    <form action="/jobs" method="POST">
+        @csrf
+        <input type="text" name="title" placeholder="Title" />
+        <input type="text" name="description" placeholder="Description" />
+        <button type="submit">Submit</button>
+    </form>
+@endsection
