456fbec4a86c60157a1060aa9a942c93b922dbd7
Abdekrahman Khakifa <bodakhalifa707@gmail.com>
2025-06-03 16:33:25 +0300

    Layout with Templete Inhertance

---
+++ resources/views/Layout.blade.php
@@ -0,0 +1,16 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Workopia | Find and List Jobs</title>
+</head>
+<body class="bg-gray-100">
+    <h1>Welcome To Workopia</h1>
+    <main class="container mx-auto p-4 mt-4">
+        @yield('content')
+    </main>
+</body>
+</html>
