37d575fb8886a0e8e919f804b2ea2f6a03df4576
Abdekrahman Khakifa <bodakhalifa707@gmail.com>
2025-06-06 22:25:48 +0300

    Layout Components & Slots

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -4,19 +4,20 @@
             <a href="{{ url('/') }}">Workopia</a>
         </h1>
         <nav class="hidden md:flex items-center space-x-4">
-            <a href="{{ url('/jobs') }}" class="text-white hover:underline py-2"
-            >All Jobs</a
-            >
-            <a href="{{ url('/jobs/saved') }}" class="text-white hover:underline py-2"
-            >Saved Jobs</a
-            >
-            <a href="{{ url('/login') }}" class="text-white hover:underline py-2"
-            >Login</a
-            >
-            <a href="{{ url('/register') }}" class="text-white hover:underline py-2"
-            >Register</a
-            >
-            <a href="{{ url('/dashboard') }}" class="text-white hover:underline py-2">
+            <a href="{{ url('/') }}"
+               class="text-white hover:underline py-2 {{ request()->is('/') ? 'text-yellow-400 font-bold' : '' }}">Home</a>
+            <a href="{{ url('/jobs') }}"
+               class="text-white hover:underline py-2 {{ request()->is('jobs') ? 'text-yellow-400 font-bold' : '' }}">All
+                Jobs</a>
+            <a href="{{ url('/jobs/saved') }}"
+               class="text-white hover:underline py-2 {{ request()->is('jobs/saved') ? 'text-yellow-400 font-bold' : '' }}">Saved
+                Jobs</a>
+            <a href="{{ url('/login') }}"
+               class="text-white hover:underline py-2 {{ request()->is('login') ? 'text-yellow-400 font-bold' : '' }}">Login</a>
+            <a href="{{ url('/register') }}"
+               class="text-white hover:underline py-2 {{ request()->is('register') ? 'text-yellow-400 font-bold' : '' }}">Register</a>
+            <a href="{{ url('/dashboard') }}"
+               class="text-white hover:underline py-2 {{ request()->is('dashboard') ? 'text-yellow-400 font-bold' : '' }}">
                 <i class="fa fa-gauge mr-1"></i> Dashboard
             </a>
             <a
