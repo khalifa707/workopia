891e5f043a622017c183a7105e9be970b6092a23
Abdekrahman Khakifa <bodakhalifa707@gmail.com>
2025-06-08 21:37:16 +0300

    Input Validations & Errors

--- resources/views/jobs/create.blade.php
+++ resources/views/jobs/create.blade.php
@@ -1,10 +1,18 @@
 <x-layout>
     <x-slot name="title">Create Job</x-slot>
     <h1>Create Job</h1>
-        <form action="/jobs" method="POST">
+    <form action="/jobs" method="POST">
         @csrf
-        <input type="text" name="title" placeholder="Title" />
-        <input type="text" name="description" placeholder="Description" />
+        <input type="text" name="title" placeholder="Title" value="{{ old('title') }}">
+        <!-- Error Message for Title -->
+        @error('title')
+        <div class="text-red-500 mt-2 text-sm">{{ $message }}</div>
+        @enderror
+        <input type="text" name="description" placeholder="Description" value="{{ old('description') }}">
+        <!-- Error Message for Description -->
+        @error('description')
+        <div class="text-red-500 mt-2 text-sm">{{ $message }}</div>
+        @enderror
         <button type="submit">Submit</button>
-        </form>
+    </form>
 </x-layout>
