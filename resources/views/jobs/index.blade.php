57ee1f30244682b0596c0aef0782c8d2bdae2b9a
Abdekrahman Khakifa <bodakhalifa707@gmail.com>
2025-06-08 17:40:37 +0300

    Fetching Data & Elequent ORM

--- resources/views/jobs/index.blade.php
+++ resources/views/jobs/index.blade.php
@@ -1,15 +1,9 @@
 <x-layout>
-<h1>{{$title}}</h1>
-@if($jobs->count() > 0)
-<ul>
-    @foreach($jobs as $job)
-        <li>
-            <h3>{{$job->title}}</h3>
-            <p>{{$job->description}}</p>
-        </li>
-    @endforeach
-</ul>
-@else
-<p>No jobs available</p>
-@endif
+    <ul>
+        @forelse($jobs as $job)
+            <li>{{ $job->title }} - {{$job->description}}</li>
+        @empty
+            <li>No jobs found</li>
+        @endforelse
+    </ul>
 </x-layout>
